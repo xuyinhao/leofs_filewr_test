0614a841cc1e1ee95c9ca59df856901aa55eb4ff
yinhaoxu <yinhaoxu@hotmail.com>
2019-03-16 22:52:44 +0800

    add task text

--- sbin/p.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python
-#-*- coding:utf-8 -*-
-import multiprocessing
-import os,sys,subprocess
-import time
-
-hname=subprocess.Popen("hostname",shell=True,stdout=subprocess.PIPE)
-print ''.join(hname.stdout.readlines())
-
