a4d4766babbf2fb399114a2b5d173eea7859709c
ericxu <33274770+xuyinhao@users.noreply.github.com>
2019-03-16 23:24:07 +0800

    Delete t.sh

--- bin/t.sh
+++
@@ -1,8 +0,0 @@
-vols=`fdisk -l |grep "Disk /dev/leobd1d${1}"|awk '{print $5}'`
-         blksize=`expr $vols / 1024 / 1024`
-
-if [ $blksize -lt 2048 ];then
-                smallfnum=10000
-                sm=1
-         fi
-echo $sm
